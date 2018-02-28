d716b2ea2701bf1a72801ccc00cb5623e1b0f81a
Øyvind Bertelsen <roberto.oyvind@gmail.com>
2018-02-28 23:32:08 +0100

    De kan deklareres med en gang

--- naiveBayes.py
+++ naiveBayes.py
@@ -5,8 +5,8 @@ import numpy as np
 posTrainPath = "\\Data\\train\\pos\\"  # relative path to positive train positive reviews, make sure data folder is in same directory as this py file.
 negTrainPath = "\\Data\\train\\pos\\"
 
-posTrainFiles = list  # list of files
-negTrainFiles = list  # list of files
+posTrainFiles = directoryExplorer.getTrainData(posTrainPath)# list of files
+negTrainFiles = directoryExplorer.getTrainData(negTrainPath)  # list of files
 
 posWords = None
 negWords = None
@@ -16,6 +16,6 @@ def addWords(path):
 	return path
 
 
-posTrainFiles = directoryExplorer.getTrainData(posTrainPath)
+
 print(posTrainFiles)
 # print(addWords())
