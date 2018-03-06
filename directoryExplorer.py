580fc5b62e33a6a8ce8af7992e0aaa76b2606dac
Øyvind Bertelsen <roberto.oyvind@gmail.com>
2018-03-06 16:20:06 +0100

    No need for extra variable

--- directoryExplorer.py
+++ directoryExplorer.py
@@ -23,17 +23,16 @@ def getwords(listWithPaths):
 	:param listWithPaths: a list containing the paths of the txt files
 	:return: a list containing the words
 	"""
-	br="br"
 	finalListOfWords = []
 	for path in listWithPaths:
 		with open(path, encoding = "utf8") as file:
 			text = file.read().lower()
 			file.close()
-			text = re.sub('[\'()/!.":,]', '', text)  # remove characters we dont want
 			text = re.sub('[<>]', ' ', text)
+			text = re.sub('[<>]', ' ', text) # adding space where < or > exists to separate br tags from words
 			words = list(text.split())
 			for word in words:
-				if word.__len__() > 1 and word not in br:  # check if word is more than one character and is not br which is from the html markup
+				if word.__len__() > 1 and word not in "br":  # check if word is more than one character and is not br which is from the html markup
 					finalListOfWords.append(word)
 	return finalListOfWords
 
