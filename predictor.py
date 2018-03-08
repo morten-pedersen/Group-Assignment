8662312354158c6cbecffd6578f336cbba8853d1
morten-pedersen <mortenpedersennn1989@gmail.com>
2018-03-08 13:40:02 +0100

    more testtext

--- predictor.py
+++ predictor.py
@@ -1,13 +1,26 @@
-import fileExplorer
-import naiveBayes
-import os
 import re
 
-
-def testText (path): # Opens a single textfile with path
-	with open(path, encoding = "utf8") as file:
-		text = file.read().lower()
+def openTestText ():
+	listOfTestWords = [] # Opens a single textfile with path
+	with open("C:\\Users\\morte\\PycharmProjects\\Group-Assignment\\Data\\test\\pos\\25_10.txt", encoding = "utf8") as file:
+		testText = file.read().lower()
 		file.close()
-		text = re.sub('[\'()/!.":,!?]', '', text)
-		text = re.sub('[<>]', ' ', text)
-	return testText()
+		testText = re.sub('[\'()/!.":,!?]', '', testText)
+		testText = re.sub('[<>]', ' ', testText)
+		testWords = list(testText.split())
+		for testWord in testWords:
+			if testWord.__len__() > 1 and testWord not in "br":
+				listOfTestWords.append(testWord)
+	return listOfTestWords()
+
+def testTextFrequency(listOfTestWords):
+	testDictionary = {}
+	for testWord in openTestText():
+		if testWord in testDictionary:
+			testDictionary[testWord] += 1
+		else:
+			testDictionary[testWord] = 1
+	return testDictionary
+
+
+
