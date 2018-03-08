5b338f27478f1f41a6d390e29cbf390bc2e7e523
morten-pedersen <mortenpedersennn1989@gmail.com>
2018-03-08 12:34:11 +0100

    new stuff
    added negative word counter and predictor class

--- naiveBayes.py
+++ naiveBayes.py
@@ -1,6 +1,7 @@
 import fileExplorer
 import numpy as np
 import os
+import math
 
 posTrainPath = os.getcwd() + "\\Data\\train\\pos\\"  # relative path to positive train positive reviews, make sure data folder is in same directory as this py file.
 negTrainPath = os.getcwd() + "\\Data\\train\\neg\\"
@@ -18,7 +19,12 @@ def addWords(path):
 
 posTrainPathList = fileExplorer.getfilelist(posTrainPath)
 posWords = fileExplorer.getwords(posTrainPathList)
+negTrainPathList = fileExplorer.getfilelist(negTrainPath)
+negWords = fileExplorer.getwords(negTrainPathList)
 frequency = fileExplorer.makeWordFrequnencyList(posWords)
+negFrequency = fileExplorer.makeWordFrequnencyList(negWords)
 commonWords = fileExplorer.getCommonWords(frequency, 50)
 print("Test complete.")
 print(commonWords)
+
+
---
+++ predictor.py
@@ -0,0 +1,5 @@
+import fileExplorer
+import naiveBayes
+import os
+import re
+
