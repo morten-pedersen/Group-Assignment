b550c4f6c6212617b49cec262b3ef5b55b5d08d0
morten-pedersen <mortenpedersennn1989@gmail.com>
2018-03-08 13:05:06 +0100

    fileopener for single textfile (to be used for predictions)

--- predictor.py
+++ predictor.py
@@ -3,3 +3,11 @@ import naiveBayes
 import os
 import re
 
+
+def testText (path): # Opens a single textfile with path
+	with open(path, encoding = "utf8") as file:
+		text = file.read().lower()
+		file.close()
+		text = re.sub('[\'()/!.":,!?]', '', text)
+		text = re.sub('[<>]', ' ', text)
+	return testText()
