87b9c7687e26ee50574e342687ea487ed49a29ed
Øyvind Bertelsen <roberto.oyvind@gmail.com>
2018-03-18 02:16:13 +0100

    Predict everything in the test set (#22)
    * give fileHandler import a shorter name
    
    * Rough outline of how we get all the test reviews
    
    * Fix path bug
    
    * Add test for TestdataProcessing
    
    * Changed while loop and fixed error from making variables global
    
    * Fix path bug
    
    * Added doc to function that returns the test data
    
    * Fixed bug with reviews being added to dicts
    
    * Renamed returned dictionary in getInitializedTestData function
    
    * Change makeClassPrediction to make it usable for the entire test set
    
    path is None by default and if it is None the function will not attempt to get words out of the path, cause there is no path.
    
    * Reformat code
    
    * Remove old function and TODO
    
    * Implemented function that will try to predict every review in the test set
    
    It will return a dictionary with the result
    
    * Implemented test for predicting all the test reviews
    
    * Fixed multiple bugs
    
    - KeyError bug
    - Missing counter
    - Counter wasn't being incremented
    - Counter wasn't being reset
    - Hack fix for missing wordFrequency - this could use a more elegant solution
    
    * Fixed another bug
    
    - Ended up only going through half the reviews because the counter is counting up and a review is removed every time so they meet in the middle.
    
    * Updated old tests to run with the changes made to makeClassPrediction
    
    * Changed some of the prints in the tests
    
    * Try to improve error rate by using more decimals
    
    * Error rate is now calculated in the test

--- predictor.py
+++ predictor.py
@@ -1,6 +1,7 @@
-import os
-import fileHandler
 import math
+import os
+
+import fileHandler as fh
 
 
 def getInitializedTrainData():
@@ -16,13 +17,13 @@ def getInitializedTrainData():
 	os.chdir(dir_path)
 	posTrainPath = os.getcwd() + "\\Data\\train\\pos\\"  # relative path to positive train positive reviews, make sure data folder is in same directory as this py file.
 	negTrainPath = os.getcwd() + "\\Data\\train\\neg\\"
-	posTrainFiles = fileHandler.getfilelist(posTrainPath)  # list of files
-	negTrainFiles = fileHandler.getfilelist(negTrainPath)  # list of files
-	posWords = fileHandler.getwords(posTrainFiles)  # list of words
-	negWords = fileHandler.getwords(negTrainFiles)  # list of words
-	posFrequency = fileHandler.makeWordFrequencyDict(
+	posTrainFiles = fh.getfilelist(posTrainPath)  # list of files
+	negTrainFiles = fh.getfilelist(negTrainPath)  # list of files
+	posWords = fh.getwords(posTrainFiles)  # list of words
+	negWords = fh.getwords(negTrainFiles)  # list of words
+	posFrequency = fh.makeWordFrequencyDict(
 		posWords)  # dictionary with frequency of words found in positive reviews
-	negFrequency = fileHandler.makeWordFrequencyDict(
+	negFrequency = fh.makeWordFrequencyDict(
 		negWords)  # dictionary with frequency of words found in negative reviews
 	posProbability = posTrainFiles.__len__()/(posTrainFiles.__len__() + negTrainFiles.__len__())  # baseline prob
 	negProbability = negTrainFiles.__len__()/(posTrainFiles.__len__() + negTrainFiles.__len__())  # .50ish?
@@ -33,7 +34,96 @@ def getInitializedTrainData():
 	return initializedTrainingData
 
 
-def makeClassPrediction(path, wordCountDict, priorProb):
+def getIntitializedTestData():
+	"""
+	Test data is gathered, processed and put in dictionaries
+	:return: dict with positive and negative reviews
+	Keys:
+	posReviews - the positive reviews
+	negReviews - the negative reviews
+	"""
+	dir_path = os.path.dirname(os.path.realpath(__file__))  # get the path to python file
+	os.chdir(dir_path)
+	posTrainPath = os.getcwd() + "\\Data\\test\\pos\\"  # relative path to positive train positive reviews, make sure data folder is in same directory as this py file.
+	negTrainPath = os.getcwd() + "\\Data\\test\\neg\\"
+	posTrainFiles = fh.getfilelist(posTrainPath)  # list of files
+	negTrainFiles = fh.getfilelist(negTrainPath)  # list of files
+	i = 0
+	posReviews = {}
+	negReviews = {}
+	while posTrainFiles.__len__() is not 0:  # while list is not empty get reviews and put them into a dict
+		review = fh.getwords(path = posTrainFiles.pop())
+		posReviews[i] = review  # key is just a number, use __len__() on the dict to find number of reviews later
+		i += 1
+	i = 0
+	while negTrainFiles.__len__() is not 0:
+		review = fh.getwords(path = negTrainFiles.pop())
+		negReviews[i] = review
+		i += 1
+
+	initializedTestData = {"posReviews":posReviews, "negReviews":negReviews}
+	return initializedTestData
+
+
+def predictTestReviews(trainingData = getInitializedTrainData(), testData = getIntitializedTestData()):
+	"""
+	This function will go through all set of test reviews and attempt to predict if it is positive or negative and save the results
+	:param trainingData: dict that looks like this {"posFreq":posFrequency, "negFreq":negFrequency,
+							  					    "posProb":posProbability, "negProb":negProbability}
+	:param testData: dict that looks like this {"posReviews":posReviews, "negReviews":negReviews}
+	:return: a dictionary containing the results, key strings are:
+	predictedPositive
+	predictedNegative
+	correctPredictions
+	incorrectPredictions
+	they contain integers
+	"""
+	predictedPositive = 0
+	predictedNegative = 0
+	correctPredictions = 0
+	incorrectPredictions = 0
+	posTrainingFreq = trainingData["posFreq"]
+	negTrainingFreq = trainingData["negFreq"]
+	posTestReviews = testData["posReviews"]
+	negTestReviews = testData["negReviews"]
+	counter = 0
+	while posTestReviews.__len__() > counter:
+		currentReview = posTestReviews[counter]
+		posPrediction = makeClassPrediction(text = currentReview, wordCountDict = posTrainingFreq, priorProb = 0.5)
+		negPrediction = makeClassPrediction(text = currentReview, wordCountDict = negTrainingFreq, priorProb = 0.5)
+
+		if finalPrediction(posPrediction, negPrediction) is "positive":
+			correctPredictions += 1
+			predictedPositive += 1
+		else:
+			incorrectPredictions += 1
+			predictedNegative += 1
+		counter += 1
+	counter = 0
+	while negTestReviews.__len__() > counter:
+		currentReview = negTestReviews[counter]
+		posPrediction = makeClassPrediction(text = currentReview, wordCountDict = posTrainingFreq, priorProb = 0.5)
+		negPrediction = makeClassPrediction(text = currentReview, wordCountDict = negTrainingFreq, priorProb = 0.5)
+
+		if finalPrediction(posPrediction, negPrediction) is "negative":
+			correctPredictions += 1
+			predictedNegative += 1
+		else:
+			incorrectPredictions += 1
+			predictedPositive += 1
+		counter += 1
+	results = {}
+	results["predictedPositive"] = predictedPositive
+	results["predictedNegative"] = predictedNegative
+	results["correctPredictions"] = correctPredictions
+	results["incorrectPredictions"] = incorrectPredictions
+	return results
+
+
+# TODO priorprob = log(0.5)????
+
+
+def makeClassPrediction(wordCountDict, priorProb, path = None, text = None):
 	"""
 	This function will predict if the review is positive or negative, depending on what is given as arguments
 	:param path: the path to the txt file
@@ -41,14 +131,18 @@ def makeClassPrediction(path, wordCountDict, priorProb):
 	:param priorProb: the prior probability for positive or negative review, should be around 0.50
 	:return: a float with 2 decimals
 	"""
-	countedText = fileHandler.getwords(path = path)  # get the words from the file
-	countedText = fileHandler.makeWordFrequencyDict(countedText)  # make a dict with word frequency in given file
+	if path is None:  # hack - TODO fix this later
+		countedText = text
+		countedText = fh.makeWordFrequencyDict(countedText)
+	if path is not None:  # hack - TODO fix this later
+		countedText = fh.getwords(path = path)  # get the words from the file
+		countedText = fh.makeWordFrequencyDict(countedText)  # make a dict with word frequency in given file
 	prediction = 0  # declare prediction
 	for word in countedText:  # go through every word
 		if word in wordCountDict:  # skip those words we don't have seen before
 			prediction += (wordCountDict[word]*(countedText[word] + 1))  # multiply word frequency
 	prediction = priorProb*prediction  # multiply prior probability
-	return round(math.log10(prediction), 2)  # return float with 2 decimals
+	return round(math.log10(prediction), 5)  # return float with 5 decimals
 
 
 def finalPrediction(posPrediction, negPrediction):
--- testing.py
+++ testing.py
@@ -1,12 +1,15 @@
 import os
-import predictor as predict
 import time
+
+import predictor as predict
 
 
 def testingPredictions():
 	"""
 	This function allow us to test quickly what the predictions are
 	"""
+	dir_path = os.path.dirname(os.path.realpath(__file__))  # get the path to python file
+	os.chdir(dir_path)
 	posReviewPath = os.getcwd() + "\\Data\\test\\pos\\1_10.txt"  # positive review
 	negReviewPath = os.getcwd() + "\\Data\\test\\neg\\0_2.txt"  # negative review
 	trainingData = predict.getInitializedTrainData()
@@ -16,25 +19,52 @@ def testingPredictions():
 	negProbability = trainingData["negProb"]
 	print("Predicting if a positive review is positive or negative...")
 	print("positive prediction is")
-	posPrediction = predict.makeClassPrediction(posReviewPath, posFrequency, posProbability)
+	posPrediction = predict.makeClassPrediction(path = posReviewPath, wordCountDict = posFrequency, priorProb = posProbability)
 	print(posPrediction)
 	print("negative prediction is")
-	negPrediction = predict.makeClassPrediction(posReviewPath, negFrequency, negProbability)
+	negPrediction = predict.makeClassPrediction(path =posReviewPath, wordCountDict =negFrequency, priorProb =negProbability)
 	print(negPrediction)
 	prediction = predict.finalPrediction(posPrediction, negPrediction)
 	print("It is a " + prediction + " review")
 	print("")
 	print("Predicting if a negative review is positive or negative...")
 	print("positive prediction is")
-	posPrediction = predict.makeClassPrediction(negReviewPath, posFrequency, posProbability)
+	posPrediction = predict.makeClassPrediction(path =negReviewPath, wordCountDict =posFrequency, priorProb =posProbability)
 	print(posPrediction)
 	print("negative prediction is")
-	negPrediction = predict.makeClassPrediction(negReviewPath, negFrequency, negProbability)
+	negPrediction = predict.makeClassPrediction(path =negReviewPath, wordCountDict =negFrequency, priorProb =negProbability)
 	print(negPrediction)
 	prediction = predict.finalPrediction(posPrediction, negPrediction)
 	print("It is a " + prediction + " review")
 
 
+def testingTestDataProcessing():
+	trainingData = predict.getIntitializedTestData()
+	return trainingData
+
+
+def testPredicTestReviews():
+	return predict.predictTestReviews()
+
+
+startTime = time.time()
+print("Running testPredicTestReviews...")
+results = testPredicTestReviews()  # running the test
+print(results)
+numberOfReview = 25000
+print(str((results["correctPredictions"]- numberOfReview)/numberOfReview*(100)*-1) + "% is the error rate ")
+endTime = time.time()
+print("Test is complete.")
+print("Test took: " + str(round(endTime - startTime, 2)) + " sec")
+print()
+startTime = time.time()
+print("Running testingTestDataProcessing...")
+trainingData = testingTestDataProcessing()  # running the test
+print("Data was processed")
+endTime = time.time()
+print("Test is complete.")
+print("Test took: " + str(round(endTime - startTime, 2)) + " sec")
+print()
 startTime = time.time()
 print("Running testingPredictions...")
 testingPredictions()  # running the test
